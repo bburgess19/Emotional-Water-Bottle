78af34a9e3b1501b0c60b20155636eab03aa1f01
Ben Burgess <bburgess19@cs.uchicago.edu>
2024-03-08 20:04:11 -0600

    Final push

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -7,14 +7,32 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 #define OLED_RESET     -1
-#define SCREEN_ADDRESS 0x3D
+#define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 int LED_PIN = 1;
+int BUTTON_PIN = 2;
+enum CalibrationState { Initial, CalibrateLow, CalibrateHigh };
+
+/*** GLOBAL CONSTANTS ***/
+const int GESTURE_DURATION = 1000;
+const int GESTURE_COOLDOWN = 200;
+const int DIAG_DURATION = 3000;
+const int MAX_PING_INTERVAL = 1000;
 
 /*** STATE VARIABLES ***/
-unsigned long outOfRangePingCount = 0;
-unsigned long waterLevel = 0;
-unsigned long happinessLevel = 0;
+unsigned long lastPingTime = 0;
+unsigned long gestureStartTime = 0;
+unsigned long gestureCooldownStartTime = 0;
+unsigned long diagStartTime = 0;
+int waterLevel = 0;
+int happinessLevel = 0;
+bool isOn = false;
+bool isCalibrating = false;
+bool isDiagShowing = false;
+bool didStartGesture = false;
+bool didReleaseDuringGesture = false;
+bool didNotifyDistanceError = false;
+CalibrationState calibrationState = Initial;
 
 void setup() {
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -27,23 +45,58 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+
   if (Serial1.available()) {
-    if (Serial1.read() == 255) {
-      waterLevel = int(Serial1.read());
-      happinessLevel = int(Serial1.read());
-      sendAck();
-      clearDiagnostics();
-      printDiagnostics();
-      digitalWrite(LED_PIN, HIGH);
-      delay(100);
-      digitalWrite(LED_PIN, LOW);
+    if (isOn) {
+      checkForDisconnection();
+
+      if (Serial1.read() == 255) {
+        byte code = Serial1.read();
+
+        switch (code) {
+          case 'a':
+            break; // empty to allow KEEPALIVE messages
+          case 'p':
+            waterLevel = int(Serial1.read());
+            happinessLevel = int(Serial1.read());
+            sendAck();
+            break;
+          
+          case 'v':
+            // Vibrate
+            break;
+
+          lastPingTime = currentTime;
+          didNotifyDistanceIssue = false;
+        }
+        digitalWrite(LED_PIN, HIGH);
+        delay(100);
+        digitalWrite(LED_PIN, LOW);
+      }
+    } else {
+      // Empty the buffer, no acknowledgments
+      while (Serial1.available()) { Serial1.read(); }
     }
   }
+
+  if (isDiagShowing && currentTime - diagStartTime > DIAG_DURATION) {
+    hideDiagnostics();
+  }
+
+  handleButtonGesture();
 }
 
 /* Sends an acknowledgement to the server */
 void sendAck() {
   Serial1.write(255);
+  Serial1.write('a');
+}
+
+/* Hides diagnostics data, resetting any state variables */
+void hideDiagnostics() {
+  isDiagShowing = false;
+  clearDiagnostics();
 }
 
 /* Clears the diagnostics */
@@ -55,7 +108,150 @@ void clearDiagnostics() {
 void printDiagnostics() {
     display.setTextSize(2);
     display.setTextColor(WHITE);
-    display.setCursor(60, 0);
+    display.setCursor(0, 10);
+    display.print("Water %: ");
+    display.print(waterLevel);
+    display.print(0, 20);
+    display.print("Happiness: ");
     display.print(happinessLevel);
     display.display();
+    isDiagShowing = true;
+}
+
+/* Handles all button gestures.
+ *
+ * The functionality is twofold:
+ * 1. Show diagnostics with a single button press
+ * 2. Start calibration with a double button press
+ * 3. Turn off pairing with a long press
+ */
+void handleButtonGesture() {
+  unsigned long currentTime = millis();
+  int isHeld = digitalRead(BUTTON_PIN);
+  
+  // Exit early for a cooldown
+  if (currentTime - gestureCooldownStartTime < GESTURE_COOLDOWN)
+    return;
+
+  // Begin a gesture if it hasn't started yet
+  if (isHeld && !didStartGesture) {
+    didStartGesture = true;
+    gestureStartTime = currentTime;
+    return;
+  }
+
+  // Exit early if this is not a gesture
+  if (!didStartGesture) return;
+
+  if (isHeld) {
+    // End the gesture after the timeout
+    if (currentTime - gestureStartTime > GESTURE_DURATION) {
+      if (isCalibrating) {
+        sendCalibrationPing();
+      }
+
+      // We short circuit the double-press gesture, so we only have to check long presses
+      // or just a regular press here
+      if (!didReleaseDuringGesture && isHeld) {
+        togglePower();
+      } else {
+        printDiagnostics();
+      }
+
+      gestureCooldownStartTime = currentTime;
+    }
+
+    if (didReleaseDuringGesture) {
+      if (isOn && !isCalibrating) {
+        // Start calibration
+        beginCalibration();
+        didStartGesture = false;
+      }
+    }
+  } else {
+      didReleaseDuringGesture = true;
+  }
+}
+
+/* Initializes calibration mode */
+void beginCalibration() {
+  isCalibrating = true;
+  calibrationState = Initial;
+  drawCalibrationState();
+}
+
+/* Sends the calibration ping to the bottle */ 
+void sendCalibrationPing() {
+  Serial1.write(255);
+  Serial1.write('c');
+
+  switch (calibrationState) {
+    case Initial:
+      calibrationState = CalibrateLow;
+      break;
+    case CalibrateLow:
+      calibrationState = CalibrateHigh;
+      break;
+    case CalibrateHigh:
+      isCalibrating = false;
+      break;
+  }
+  drawCalibrationState();
+}
+
+/* Toggles the wristbands power state */
+void togglePower() {
+  if (isOn) {
+    isOn = false;
+  } else {
+    isCalibrating = false;
+    isOn = true;
+  }
+}
+
+/* Vibrates wristband */
+void vibrate()
+{
+  // TODO
+}
+
+/* Checks if too much time has passed since receiving a message */
+void checkForDisconnection() {
+  unsigned long currentTime = millis();
+  if (MAX_PING_INTERVAL - currentTime > MAX_PING_INTERVAL && !didNotifyDistanceError) {
+    notifyDistanceIssue();
+    vibrate();
+  }
+}
+
+/* Notifies wearer that they are out of range */
+void notifyDistanceIssue() {
+  display.clear();
+  display.setTextSize(2);
+  display.setCursor((0, 0);
+  display.print("Don't forget me!");
+  display.display();
+  vibrate();
+  didNotifyDistanceError = false;
+}
+
+/* Draws the instructions for the calibration states */
+void drawCalibrationState() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+
+  switch (calibrationState) {
+    case Initial:
+      display.print("Place empty bottle");
+      display.display();
+      break;
+    case CalibrateLow:
+      display.print("Place full bottle");
+      display.display();
+      break;
+    default:
+      display.clearDisplay();
+      break;
+  }
 }
--- sender/sender.ino
+++ sender/sender.ino
@@ -1,14 +1,25 @@
 #include "math.h"
 #include "HX711.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #define LOADCELL_DOUT_PIN  0
 #define LOADCELL_SCK_PIN  8
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 enum ConnectionState { CONNECTED, DISCONNECTED };
+enum CalibrationState { Initial, CalibrateLow, CalibrateHigh };
 
 /*** GLOBAL CONSTANTS ***/
 const int PING_TIMEOUT = 200;
 const int PING_MAX = 5;
-const int HAPPINESS_COOLDOWN = 1000;
+const int HAPPINESS_COOLDOWN = 1000;// * 60;
 const float SIP_THRESHOLD = 0.1;
 
 /*** STATE VARIABLES ***/
@@ -18,9 +29,10 @@ unsigned long lastHappinessCooldownTime = 0;
 unsigned long outOfRangePingCount = 0;
 int waterLevel = 0;
 int happinessLevel = 0;
-int bottleMin = 0;
-int bottleMax = 0;
-bool isCalibrationMode = false;
+float bottleMin = 0;
+float bottleMax = 0;
+bool isCalibrating = false;
+CalibrationState calibrationState = Initial;
 bool isBottleLifted = false;
 
 /*** LOAD CELL VARIABLES ***/
@@ -29,17 +41,21 @@ float calibration_factor = -110000;
 
 void setup() 
 {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
   Serial1.begin(115200);
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale();
   scale.tare();
+  display.display();
 }
 
 void loop() 
 {
   unsigned long currentTime = millis();
-  // Every 200ms, always send [255, x, y]
-
   // Change to disconnected state if we haven't received a ping in a while
   if (outOfRangePingCount > PING_MAX) {
     state = DISCONNECTED;
@@ -58,20 +74,17 @@ void loop()
  */
 void updateStatus()
 {
-  if (isCalibrationMode) return;
+  if (isCalibrating) return;
 
   unsigned long currentTime = millis();
-  if (!isCalibrationMode) {
-    int weight = scale.get_units(10);
-    // Serial.print("Read weight: ");
-    // Serial.print(weight);
-    // Serial.print(" Water level: ");
-    // Serial.println(waterLevel);
+  if (!isCalibrating) {
+    float weight = scale.get_units(10);
+    waterLevel = (weight - bottleMin) / (bottleMax - bottleMin) * 100;
     if (weight < waterLevel - SIP_THRESHOLD) {
-      happinessLevel += 20; // Increase happiness if the user drinks water
+      happinessLevel = min(happinessLevel + 20, 100); // Increase happiness if the user drinks water
       lastHappinessCooldownTime = currentTime;
     } else if (currentTime - lastHappinessCooldownTime > HAPPINESS_COOLDOWN) {
-      happinessLevel--; // Decrease happiness if the user hasn't drank water in a while
+      happinessLevel = max(happinessLevel - 1, 0); // Decrease happiness if the user hasn't drank water in a while
       lastHappinessCooldownTime = currentTime;
     }
   }
@@ -84,19 +97,30 @@ void pingWristband(unsigned long currentTime)
 {
   lastPingTime = currentTime;
   sendStatus();
-  checkForAck();
+  checkForMessage();
 }
 
 /*
- * Checks for an acknowledgement from the wristband
+ * Checks for messages from the wristband
  */
-void checkForAck()
+void checkForMessage()
 {
   if (Serial1.available()) {
     if (Serial1.read() == 255) {
-      outOfRangePingCount = 0;
-      state = CONNECTED;
-      Serial.println("ack");
+      byte code = Serial1.read();
+
+      switch (code) {
+        case 'a':
+          handleAck();
+          break;
+        case 'c':
+          if (!isCalibrating) {
+            beginCalibration();
+          } else {
+            calibrationHandler();
+          }
+          break;
+      }
     } else { 
       outOfRangePingCount++;
     }
@@ -111,4 +135,102 @@ void sendStatus()
   Serial1.write(255);
   Serial1.write(waterLevel);
   Serial1.write(happinessLevel);
+}
+
+/* Handles the acknowledgement from the wristband */
+void handleAck() {
+  outOfRangePingCount = 0;
+  state = CONNECTED;
+  Serial.println("ack");
+}
+
+/* Handles a calibration message */
+void calibrationHandler() {
+  int weight = scale.get_units(10);
+  switch (calibrationState) {
+    case Initial:
+      bottleMin = weight;
+      drawCalibrationState();
+      break;
+    case CalibrateLow:
+      bottleMax = weight;
+      drawCalibrationState();
+      break;
+    default:
+      isCalibrating = false;
+      drawFace();
+  }
+}
+
+/* Initializes calibration mode */
+void beginCalibration() {
+  isCalibrating = true;
+  calibrationState = Initial;
+  drawCalibrationState();
+}
+
+/* Draws the instructions for the calibration states */
+void drawCalibrationState() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+
+  switch (calibrationState) {
+    case Initial:
+      display.print("Place empty bottle");
+      display.display();
+      break;
+    case CalibrateLow:
+      display.print("Place full bottle");
+      display.display();
+      break;
+    default:
+      display.clearDisplay();
+      break;
+  }
+}
+
+/* Draws the face for the bottle */
+void drawFace() {
+  display.clearDisplay();
+  if (happinessLevel > 66) {
+    drawHappyFace();
+  } else if (happinessLevel > 33) {
+    drawWorriedFace();
+  } else {
+    drawSadFace();
+  }
+}
+
+/* Draws the happy face */
+void drawHappyFace() {
+  display.clearDisplay();
+
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,20);
+  display.print("(^ w ^)");
+  display.display();
+}
+
+/* Draws the sad face */
+void drawSadFace() {
+  display.clearDisplay();
+
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,20);
+  display.print("(T o T)");
+  display.display();
+}
+
+/* Draws the worried face */
+void drawWorriedFace() {
+  display.clearDisplay();
+
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,20);
+  display.print("(. ~ .)");
+  display.display();
 }
